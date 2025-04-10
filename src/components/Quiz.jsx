a2d8b72a1c4db7d3dc009b7711b0da1161f3814e
Max Mariani <wmaxmariani@gmail.com>
2025-04-10 20:37:59 +0200

    Added QuestionTimer component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
-import quizCompletedImg from '../assets/quiz-complete.png';
+import quizCompletedImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -9,18 +10,19 @@ export default function Quiz() {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswer) => {
       return [...prevUserAnswer, selectedAnswer];
     });
   }
 
-  if(quizIsComplete) {
-    return <div id="summary">
-        <img src={quizCompletedImg}/>
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} />
         <h2>Quiz Completed!</h2>
-    </div>
+      </div>
+    );
   }
 
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
@@ -29,6 +31,10 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          timeout={10000}
+          onTimeout={() => handleSelectAnswer(null)}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
